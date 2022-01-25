0039e577c3f2ebf66a64f628f60ab104a7b8ab82
Cecilia Norgren <cecilia.norgren@irfu.se>
2022-01-25 15:49:44 +0100

    read trackparticlesdisordered

--- read_data/smileiReadParticleBinning.m
+++ read_data/smileiReadParticleBinning.m
@@ -1,13 +1,64 @@
 %% Development
 dirpath = '/Users/cecilia/Discs/betzy/Smilei/cn_cold_dipolarization/cold_momentum/';
+dirpath = '/Users/cno062/Discs/betzy_base/work/users/paulten/diamagnetic/';
 filename = 'ParticleBinning77.h5';
+filename = 'TrackParticlesDisordered_eon.h5';
 
 info = h5info([dirpath filename]);
 name = info.Attributes(2).Value;
+%%
 
-
+iter = 0;
 data = h5read([dirpath filename],'/timestep00001300');
 
+%% Read particles
+info.Groups(1).Groups(1).Groups(1).Groups(1).Groups(2).Datasets(1)
+nrows = 1;
+ncols = 1;
+npanels = nrows*ncols;
+h = setup_subplots(nrows,ncols);
+isub = 1;
+
+iter = 0000;
+
+
+if 1 % TrackParticlesDisordered
+  iteration = sprintf('/timestep%08.0f',iter);
+  hca = h(isub); isub = isub + 1;
+  id_all = 36:41;
+  species = cell(6,1);
+  %plot(hca,nan,nan)
+  hold(hca,'on')
+  for ii = 1:numel(id_all)
+    id = id_all(ii);
+    filename = sprintf('ParticleBinning%g.h5',id);
+    info = h5info([dirpath filename]);
+    data = h5read([dirpath filename],timestep);
+    name = h5readatt([dirpath filename],'/','name');
+    dep1_str = h5readatt([dirpath filename],'/','axis0');
+    dep1_tok = split(dep1_str);
+    if str2num(dep1_tok{5}) == 1
+      dep1 = logspace(str2num(dep1_tok{2}),str2num(dep1_tok{3}),str2num(dep1_tok{4}));
+    else
+      dep1 = linspace(str2num(dep1_tok{2}),str2num(dep1_tok{3}),str2num(dep1_tok{4}));
+    end
+    species{ii} = h5readatt([dirpath filename],'/','species');
+    hl(ii) = plot(hca,dep1,squeeze(data));
+    hca.XLabel.String = dep1_tok{1};
+    hca.YLabel.String = name(1:9);
+    hca.YLabel.Interpreter = 'none';
+    if ii == 1
+      hold(hca,'on')
+    elseif ii == numel(id_all)
+      hold(hca,'off')
+    end
+  end
+  hold(hca,'off')
+  hca.XScale = 'log';
+  hca.YScale = 'log';
+  hleg = legend(hca,species,'location','best');
+  hleg.Title.String = 'species';
+end
 
 %% Plot particle binning results
 nrows = 1;
